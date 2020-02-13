839efcb8ad6f529ae2368b9222c7a8b10a9b4a1e
Adam Arling <adam.arling@northwestern.edu>
2020-02-12 18:10:46 -0600

    Update tour dates to include ROA residency

--- src/tour-dates.js
+++ src/tour-dates.js
@@ -1,27 +1,18 @@
 export const tourDates = [
   {
-    date: "2020-02-07",
-    location: "Key West, Florida USA",
-    venue: "Key West Theater",
-    rsvp: "",
-    ticketUrl: "https://www.thekeywesttheater.com/tickets-events/",
-    venueUrl: "https://www.thekeywesttheater.com/e/abbarama-73440058191/"
-  },
-  {
-    date: "2020-02-08",
-    location: "Dania Beach, Florida USA",
-    venue: "Dania Beach Casino",
-    rsvp: "",
-    ticketUrl:
-      "https://tickets.completeticketsolutions.com/CDB/Online/default.asp?doWork::WScontent::loadArticle=Load&BOparam::WScontent::loadArticle::article_id=5D4DA9C5-F00D-48C2-9353-E025135982BF",
-    venueUrl: "https://casinodaniabeach.com/entertainment-3/"
-  },
-  {
     date: "2020-03-10",
     location: "Hollywood, California USA",
     venue: "Rock of Ages Bourbon Room",
     rsvp: "",
     ticketUrl: "https://www.eventbrite.com/e/abbarama-tickets-93022736509",
+    venueUrl: "https://www.rockofageshollywood.com/"
+  },
+  {
+    date: "2020-03-24",
+    location: "Hollywood, California USA",
+    venue: "Rock of Ages Bourbon Room",
+    rsvp: "",
+    ticketUrl: "",
     venueUrl: "https://www.rockofageshollywood.com/"
   },
   {
@@ -31,6 +22,38 @@ export const tourDates = [
     rsvp: "",
     ticketUrl: "https://www.ramsheadonstage.com/calendar/page/2/",
     venueUrl: ""
+  },
+  {
+    date: "2020-04-07",
+    location: "Hollywood, California USA",
+    venue: "Rock of Ages Bourbon Room",
+    rsvp: "",
+    ticketUrl: "",
+    venueUrl: "https://www.rockofageshollywood.com/"
+  },
+  {
+    date: "2020-04-21",
+    location: "Hollywood, California USA",
+    venue: "Rock of Ages Bourbon Room",
+    rsvp: "",
+    ticketUrl: "",
+    venueUrl: "https://www.rockofageshollywood.com/"
+  },
+  {
+    date: "2020-05-05",
+    location: "Hollywood, California USA",
+    venue: "Rock of Ages Bourbon Room",
+    rsvp: "",
+    ticketUrl: "",
+    venueUrl: "https://www.rockofageshollywood.com/"
+  },
+  {
+    date: "2020-05-19",
+    location: "Hollywood, California USA",
+    venue: "Rock of Ages Bourbon Room",
+    rsvp: "",
+    ticketUrl: "",
+    venueUrl: "https://www.rockofageshollywood.com/"
   },
   {
     date: "2020-07-10",
@@ -87,5 +110,25 @@ export const tourDates = [
     rsvp: "",
     ticketUrl: "https://metropolistouring.com/abbarama-2020/",
     venueUrl: ""
+  }
+];
+
+export const previousTourDates = [
+  {
+    date: "2020-02-07",
+    location: "Key West, Florida USA",
+    venue: "Key West Theater",
+    rsvp: "",
+    ticketUrl: "https://www.thekeywesttheater.com/tickets-events/",
+    venueUrl: "https://www.thekeywesttheater.com/e/abbarama-73440058191/"
+  },
+  {
+    date: "2020-02-08",
+    location: "Dania Beach, Florida USA",
+    venue: "Dania Beach Casino",
+    rsvp: "",
+    ticketUrl:
+      "https://tickets.completeticketsolutions.com/CDB/Online/default.asp?doWork::WScontent::loadArticle=Load&BOparam::WScontent::loadArticle::article_id=5D4DA9C5-F00D-48C2-9353-E025135982BF",
+    venueUrl: "https://casinodaniabeach.com/entertainment-3/"
   }
 ];
