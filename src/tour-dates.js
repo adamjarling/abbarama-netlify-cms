29ffbb732161649b429bb36b549dd19884b6c3c1
Adam J. Arling <aarling@gmail.com>
2021-07-05 15:07:15 -0500

    Update tour dates

--- src/tour-dates.js
+++ src/tour-dates.js
@@ -1,5 +1,16 @@
 export const tourDates = [
   {
+    date: "2021-08-27",
+    location: "DuQion, Illinois State Fair",
+    venue: "State Fair",
+    rsvp: "",
+    ticketUrl: "https://www2.illinois.gov/sites/dsf/Pages/default.aspx",
+    venueUrl: "https://www2.illinois.gov/sites/dsf/Pages/default.aspx"
+  },
+];
+
+export const previousTourDates = [
+  {
     date: "2020-03-10",
     location: "Hollywood, California USA",
     venue: "Rock of Ages Bourbon Room",
@@ -7,121 +18,6 @@ export const tourDates = [
     ticketUrl: "https://www.eventbrite.com/e/abbarama-tickets-93022736509",
     venueUrl: "https://www.rockofageshollywood.com/"
   },
-  // {
-  //   date: "2020-03-24",
-  //   location: "Hollywood, California USA",
-  //   venue: "Rock of Ages Bourbon Room",
-  //   rsvp: "",
-  //   ticketUrl: "",
-  //   venueUrl: "https://www.rockofageshollywood.com/"
-  // },
-  {
-    date: "2020-03-26",
-    location: "Annapolis, Maryland USA",
-    venue: "Rams Head On Stage",
-    rsvp: "",
-    ticketUrl: "https://www.ramsheadonstage.com/calendar/page/2/",
-    venueUrl: ""
-  },
-  // {
-  //   date: "2020-04-07",
-  //   location: "Hollywood, California USA",
-  //   venue: "Rock of Ages Bourbon Room",
-  //   rsvp: "",
-  //   ticketUrl: "",
-  //   venueUrl: "https://www.rockofageshollywood.com/"
-  // },
-  // {
-  //   date: "2020-04-21",
-  //   location: "Hollywood, California USA",
-  //   venue: "Rock of Ages Bourbon Room",
-  //   rsvp: "",
-  //   ticketUrl: "",
-  //   venueUrl: "https://www.rockofageshollywood.com/"
-  // },
-  // {
-  //   date: "2020-05-05",
-  //   location: "Hollywood, California USA",
-  //   venue: "Rock of Ages Bourbon Room",
-  //   rsvp: "",
-  //   ticketUrl: "",
-  //   venueUrl: "https://www.rockofageshollywood.com/"
-  // },
-  // {
-  //   date: "2020-05-19",
-  //   location: "Hollywood, California USA",
-  //   venue: "Rock of Ages Bourbon Room",
-  //   rsvp: "",
-  //   ticketUrl: "",
-  //   venueUrl: "https://www.rockofageshollywood.com/"
-  // },
-  {
-    date: "2020-06-25",
-    location: "Austin, Texas USA",
-    venue: "3Ten - Austin City Limits",
-    rsvp: "",
-    ticketUrl: "https://3tenaustin.com/",
-    venueUrl: "https://3tenaustin.com/calendar/abbarama?utm_source=artist"
-  },
-  {
-    date: "2020-07-10",
-    location: "Sydney, AUSTRALIA",
-    venue: "Enmore Theater",
-    rsvp: "",
-    ticketUrl: "https://metropolistouring.com/abbarama-2020/",
-    venueUrl: ""
-  },
-  {
-    date: "2020-07-11",
-    location: "Gold Coast, AUSTRALIA",
-    venue: "Southport RSL",
-    rsvp: "",
-    ticketUrl: "https://metropolistouring.com/abbarama-2020/",
-    venueUrl: ""
-  },
-  {
-    date: "2020-07-12",
-    location: "Brisbane, AUSTRALIA",
-    venue: "The Triffid",
-    rsvp: "",
-    ticketUrl: "https://metropolistouring.com/abbarama-2020/",
-    venueUrl: ""
-  },
-  {
-    date: "2020-07-15",
-    location: "Adelaide, AUSTRALIA",
-    venue: "The Gov",
-    rsvp: "",
-    ticketUrl: "https://metropolistouring.com/abbarama-2020/",
-    venueUrl: ""
-  },
-  {
-    date: "2020-07-17",
-    location: "Melbourne, AUSTRALIA",
-    venue: "Palms at Crown",
-    rsvp: "",
-    ticketUrl: "https://metropolistouring.com/abbarama-2020/",
-    venueUrl: ""
-  },
-  {
-    date: "2020-07-18",
-    location: "Melbourne, AUSTRALIA",
-    venue: "Palms at Crown",
-    rsvp: "",
-    ticketUrl: "https://metropolistouring.com/abbarama-2020/",
-    venueUrl: ""
-  },
-  {
-    date: "2020-07-19",
-    location: "Perth, AUSTRALIA",
-    venue: "Rosemont Hotel",
-    rsvp: "",
-    ticketUrl: "https://metropolistouring.com/abbarama-2020/",
-    venueUrl: ""
-  }
-];
-
-export const previousTourDates = [
   {
     date: "2020-02-07",
     location: "Key West, Florida USA",
